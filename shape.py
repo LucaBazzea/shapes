20ca74265d61c23f033a517dd3892e4840b056bf
Luca Bazzea <luca.bazzea@gmail.com>
2021-02-16 15:38:23 +0200

    Add files via upload

---
+++ shape.py
@@ -0,0 +1,14 @@
+class Quadrilateral:
+
+    def __init__(self, x, y):
+
+        self.x = x
+        self.y = y
+
+    class Trapezoid(Quadrilateral):
+
+    class Parallelogram(Quadrilateral):
+
+    class Rectangle(Quadrilateral):
+
+    class Square(Quadrilateral):
